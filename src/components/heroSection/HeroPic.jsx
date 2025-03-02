a34adaf03b198e0ae31704c7fb24059e1c44e061
imal chanaka <107134405+imalchanaka@users.noreply.github.com>
2025-03-02 13:57:48 +0530

    change nameagin

--- src/components/heroSection/HeroPic.jsx
+++ src/components/heroSection/HeroPic.jsx
@@ -2,6 +2,7 @@ import { motion } from "framer-motion";
 import { fadeIn } from "../../framerMotion/variants";
 import { PiHexagonThin } from "react-icons/pi";
 
+
 const HeroPic = () => {
   return (
     <motion.div
@@ -9,15 +10,15 @@ const HeroPic = () => {
       initial="hidden"
       whileInView="show"
       viewport={{ once: false, amount: 0 }}
-      className=" h-full flex items-center justify-center "
+      className="flex items-center justify-center h-full "
     >
       <img
-        src="../../public/images/HexaPic.png"
+        src="../../../public/images/HeroPic"
         alt="Abdelrahman Qassem"
         className="max-h-[450px] w-auto"
       />
 
-      <div className=" absolute -z-10 flex justify-center items-center animate-pulse ">
+      <div className="absolute flex items-center justify-center -z-10 animate-pulse">
         <PiHexagonThin className=" md:h-[90%] sm:h-[120%] min-h-[600px] w-auto text-cyan blur-md animate-[spin_20s_linear_infinite] " />
       </div>
     </motion.div>
--- src/components/heroSection/HeroText.jsx
+++ src/components/heroSection/HeroText.jsx
@@ -11,7 +11,7 @@ const HeroText = () => {
         viewport={{ once: false, amount: 0 }}
         className="uppercase lg:text-2xl sm:text-xl text-lightGrey "
       >
-        Front-End Web Developerees
+      Full-Stack Developer
       </motion.h2>
       <motion.h1
         variants={fadeIn("right", 0.4)}
